ec82ddac40ff2a62e84948574871ec3d7cfd94d7
frankleo <154284601@qq.com>
2023-02-16 13:56:14 +0800

    temp:临时节点标记 星期四 下午 1:56:13 二月 16日 2023 修改了 src/scripts/update_jsonfile.js

--- src/scripts/update_jsonfile.js
+++ src/scripts/update_jsonfile.js
@@ -1,4 +1,5 @@
 import path from "path";
+import pathExists from "path-exists";
 import { readFile, writeFile } from "jsonfile";
 
 import {
@@ -10,6 +11,12 @@ import {
 
 export async function update_jsonfile() {
   const package_jsonfile_path = path.resolve(process.cwd(), "./package.json");
+
+  if (!await pathExists(package_jsonfile_path)) {
+    console.log("不存在package.json");
+    return false;
+  };
+
   const package_jsonfile_content = await readFile(package_jsonfile_path);
 
   package_jsonfile_content.scripts["docker:build:test"] = `sh ${path.relative(process.cwd(), test_build_scripts_file_path)}`;
